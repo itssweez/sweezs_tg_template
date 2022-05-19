46da494c75dd2dc48d593a86c1bc59a6abfd8533
itssweez <93072996+itssweez@users.noreply.github.com>
2022-05-19 20:49:30 +0300

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -4,18 +4,9 @@ from environs import Env
 
 
 @dataclass
-class DbConfig:
-    host: str
-    password: str
-    user: str
-    database: str
-
-
-@dataclass
 class TgBot:
     token: str
     admin_ids: list[int]
-    use_redis: bool
 
 
 @dataclass
@@ -26,7 +17,6 @@ class Miscellaneous:
 @dataclass
 class Config:
     tg_bot: TgBot
-    db: DbConfig
     misc: Miscellaneous
 
 
@@ -38,13 +28,6 @@ def load_config(path: str = None):
         tg_bot=TgBot(
             token=env.str("BOT_TOKEN"),
             admin_ids=list(map(int, env.list("ADMINS"))),
-            use_redis=env.bool("USE_REDIS"),
-        ),
-        db=DbConfig(
-            host=env.str('DB_HOST'),
-            password=env.str('DB_PASS'),
-            user=env.str('DB_USER'),
-            database=env.str('DB_NAME')
         ),
         misc=Miscellaneous()
     )
